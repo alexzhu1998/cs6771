b9c27ac579a7558eafcb6c76dc75410bc3d03077
Chroosh <z5165244@cse.unsw.edu.au>
2019-07-14 12:26:02 +1000

    initial test

--- assignments/ev/euclidean_vector_test.cpp
+++ assignments/ev/euclidean_vector_test.cpp
@@ -12,3 +12,32 @@
 #include "catch.h"
 
 // TODO(students): Fill this in.
+
+
+//SCENARIO("Creation of unit vectors") {
+  //WHEN("You have two identical vectors") {
+    //EuclideanVector a{2};
+    //a[0] = 1;
+    //a[1] = 2;
+    //EuclideanVector b{2};
+    //b[0] = 2;
+    //b[1] = 1;
+    //THEN("Get their unit vectors") {
+      //EuclideanVector c{a.CreateUnitVector()};
+      //EuclideanVector d{b.CreateUnitVector()};
+      //REQUIRE(c == d);
+    //}
+  //}
+//}
+
+//SCENARIO("Accessing dimension that doesn't exist") {
+  //WHEN("You try to split an empty string") {
+    //EuclideanVector a{2};
+    //a[0] = 1;
+    //a[1] = 2;
+    //THEN("You get a single empty result") {
+      //REQUIRE_THROWS_WITH(a.at(2), "Index 2 is not valid for this EuclideanVector object");
+    //}
+  //}
+//}
+
