8fd63e3bf3ec059e68932af445aec7dfea84e760
chroosh <christopher.shi22@gmail.com>
2019-08-02 15:54:23 +1000

    graph representation

--- assignments/dg/graph.h
+++ assignments/dg/graph.h
@@ -1,14 +1,49 @@
 #ifndef ASSIGNMENTS_DG_GRAPH_H_
 #define ASSIGNMENTS_DG_GRAPH_H_
 
+#include <memory>
+#include <vector>
+
 namespace gdwg {
 
 template <typename N, typename E>
 class Graph {
  public:
-  class const_iterator {};
+  // node and edge implementation
+  struct Node;
+  struct Edge;
 
+  struct Node {
+    N value;
+    std::vector<std::weak_ptr<Edge>> out_edges;
+    std::vector<std::weak_ptr<Edge>> in_edges;
+    Node() = default;
+    Node(N node_value) : value{node_value} {};
+    
+    // TODO: Destructor
+
+  }
+
+  struct Edge {
+    std::weak_ptr<Node> dest;
+    std::weak_ptr<Node> src;
+    E weight; /* Given in template */
+
+    Edge(std::weak_ptr<Node> source, std::weak_ptr<Edge> destination, E wei) : dst{destination}, src{source}, weight{w} {};
+  
+    // TODO: Destructor
+  }
+
+  std::set<std::shared_ptr<Node>, NodeCompare> nodes;
+  std::set<std::shared_ptr<Edge>, EdgeCompare> edges;
+
+  // graph constructors
+  Graph(): nodes{}, edges{}{};
+
+  class const_iterator {};
+  
  private:
+ 
 };
 
 }  // namespace gdwg
